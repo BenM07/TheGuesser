d8bcb4831d843706c729c7bcc21c7b3f64ccb45d
BenM07 <142231528+BenM07@users.noreply.github.com>
2023-08-15 00:38:32 +0800

    Create Color.java

---
+++ src/main/java/jv/ansi/Color.java
@@ -0,0 +1,12 @@
+package jv.ansi;
+
+public class Color {
+    
+    public static String RESET = "\u001b[0m";
+    public static String BLACK = "\u001b[30m";
+    public static String RED = "\u001b[31m";
+    public static String GREEN = "\u001b[32m";
+    public static String YELLOW = "\u001B[33m";
+    public static String BLUE = "\u001b[34m";
+ 
+} // end class
